b09e3cf4ff503e68ecb0f791bda44e3759f1224d
LucasAguetai <lucas.aguetai@icloud.com>
2024-03-18 11:14:57 +0100

    new version

--- 2_Chat_with_search.py
+++ 2_Chat_with_search.py
@@ -1,14 +1,22 @@
 import streamlit as st
+import requests
+import streamlit as st
 
-from langchain.agents import initialize_agent, AgentType
-from langchain.callbacks import StreamlitCallbackHandler
-from langchain.chat_models import ChatOpenAI
-from langchain.tools import DuckDuckGoSearchRun
+siteUrl = "http://127.0.0.1:8000"
+
+userContext = None
+context = None
 
 with st.sidebar:
     model = st.selectbox('which model would you like to use',
                          ('MobileBERT', 'BERT', 'DeBERTa-v2'))
     contextSelect = st.radio("Pick a context mode:", ["File", "Text"])
+    if contextSelect == "File":
+        userContext = st.file_uploader("Pick a file for the context", accept_multiple_files=True)
+        context = "/uploadfile"
+    else:
+        userContext = st.text_input("write the context")
+        context = "/contextText"
 
 st.title("ALOQAS")
 
@@ -24,15 +32,20 @@ if "messages" not in st.session_state:
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
-if prompt := st.chat_input(placeholder="Who won the Women's U.S. Open in 2018?"):
+if prompt := st.chat_input(placeholder="Write to ALOQAS..."):
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
+    params = {'texte': prompt, "model": model}
+    if userContext is not None:
+        if contextSelect == "File" and userContext:
+            # Préparation de la liste des fichiers pour l'envoi
+            files = [("file", (file.name, file, file.type)) for file in userContext]
+            response = requests.post(siteUrl + context, params=params, files=files)
+        else:
+            params["context"] = userContext
+            response = requests.post(siteUrl + context, params=params)
+    else:
+        response = requests.post(siteUrl + '/withoutFile', params=params)
 
-    with st.chat_message("assistant"):
-        st_cb = StreamlitCallbackHandler(
-            st.container(), expand_new_thoughts=False)
-        response = search_agent.run(
-            st.session_state.messages, callbacks=[st_cb])
-        st.session_state.messages.append(
-            {"role": "assistant", "content": response})
-        st.write(response)
+    st.write("Statut de la requête:", response.status_code)
+    st.write("Réponse du serveur:", response.text)
